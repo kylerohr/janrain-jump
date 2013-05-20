1dd6e1052ba70e55269a233203e12691dab87160
Kyle Rohr <krohr@janrain.com>
2013-05-20 14:28:13 -0700

    Added test config getters and setters

--- app/code/local/Janrain/JUMP/Model/Config.php
+++ app/code/local/Janrain/JUMP/Model/Config.php
@@ -11,4 +11,15 @@ class Janrain_JUMP_Model_Config extends Mage_Core_Model_Abstract {
 					 'captureAppId' => '',
 					 'tokenUrl' => '');
 	}
+
+	function getTestConfig() {
+		// Get config
+		$config = Mage::getStoreConfig('jump/capture_settings/capture_app_id');
+		return $config;
+	}
+
+	function setTestConfig($value) {
+		// Set Config
+		Mage::getModel('core/config')->saveConfig('jump/capture_settings/capture_app_id', $value);
+	}
 }
