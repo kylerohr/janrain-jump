808226c134456657cc68cf07d099298fe18f774f
Kyle Rohr <krohr@janrain.com>
2013-06-05 16:58:49 -0700

    basic customer creation works... and revamped JumpController

--- app/code/local/Janrain/JUMP/controllers/JumpController.php
+++ app/code/local/Janrain/JUMP/controllers/JumpController.php
@@ -4,26 +4,42 @@ class Janrain_JUMP_JumpController extends Mage_Core_Controller_Front_Action
 	public function indexAction()
 	{
 		if (isset($_GET['token'])) {
-			$this->_processToken($_GET['token']);
+			$this->processToken($_GET['token']);
 		}
 	}
 
-	private function _processToken($token)
+	protected function processToken($token)
 	{
 		$uuid = Mage::app()->getRequest()->getParam('uuid');
 		$config = Mage::getModel('janrain_jump/config');
 		$configAO = new \ArrayObject($config->getConfig());
 		$api = new janrain\jump\Api($configAO);
 		$user = $api->fetchUserByUuid($uuid, $token);
-		var_dump($user);
+		//var_dump($user);
 		//echo $token;
+
+		$this->registerUser($user);
 	}
 
-	private function __registerUser($token, $uuid) {
-		
+	protected function registerUser($user) {
+		$customer = Mage::getModel('customer/customer');
+		$customer->setWebsiteId(Mage::app()->getWebsite()->getId());
+
+		$email = $user->email;
+		$customer->loadByEmail($email);
+
+		if (!$customer->getId()) {
+			$customer->setEmail($email);
+			$customer->setPassword($customer->generatePassword(16));
+			// @TODO: Set confirmed? Is this necessary?
+			$customer->save();
+		}
+
+		// @TODO: Add entry in jump table with UUID
+		// @TODO: Call loginUser()
 	}
 
-	private function __loginUser($token, $uuid) {
+	protected function loginUser($user) {
 
 	}
 }
