80f14a3afe9b70209eb2f4ef5af0f7cb74b59622
Kyle Rohr <krohr@janrain.com>
2013-06-11 15:10:50 -0700

    the new user model works

--- app/code/local/Janrain/JUMP/Model/User.php
+++ app/code/local/Janrain/JUMP/Model/User.php
@@ -3,7 +3,7 @@ require_once Mage::getBaseDir('lib') . '/janrain/PsrAutoloader.php';
 
 class Janrain_JUMP_Model_User extends Mage_Core_Model_Abstract {
 
-  function __construct() {
-    $this->_init('janrain_jump/user');
+  protected function _construct() {
+    $this->_init('jump/user');
   }
 }
